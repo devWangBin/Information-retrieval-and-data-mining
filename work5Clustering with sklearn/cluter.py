0ce4f667fbab887593c7bb00622d31bd751284d3
devWangBin <935681396@qq.com>
2018-11-30 14:11:42 +0800

    Create cluter.py

---
+++ work5Clustering with sklearn/cluter.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 09:50:40 2018
+
+@author: 93568
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
